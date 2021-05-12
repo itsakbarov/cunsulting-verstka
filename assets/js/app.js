4285d204a4e4b080e269a63849b94ecd3f3588c3
sardor akbarov <sardor@novalab.uz>
2021-05-12 22:01:47 +0500

    base styles added

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,20 +13,24 @@ $(window).scroll(function () {
 const $menuBtn = document.querySelector(".mobile-menu");
 const $Appbar = document.querySelector(".appbar");
 const $Overlay = document.querySelector(".overlay");
+const $Body = document.querySelector("body")
 
 $(".mobile-menu").click(() => {
     $menuBtn.classList.add("open");
     $Appbar.classList.add("open");
     $Overlay.classList.add("open");
+    $Body.classList.add("fix");
 })
 
 $(".close-btn").click(() => {
     $menuBtn.classList.remove("open");
     $Appbar.classList.remove("open");
     $Overlay.classList.remove("open");
+    $Body.classList.remove("fix");
 })
 $($Overlay).click(() => {
     $menuBtn.classList.remove("open");
     $Appbar.classList.remove("open");
     $Overlay.classList.remove("open");
+    $Body.classList.remove("fix");
 })
